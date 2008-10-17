4da2c58a4e21f00a4fd9153e1ca91da9543df6ef
manuel <manuel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-10-17 15:31:43 +0000

    moviepcplot.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/archetypes@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/archetypes-movie.R
+++ pkg/R/archetypes-movie.R
@@ -1,6 +1,6 @@
 
 movieplot <- function(zs, data, show=c('atypes', 'adata'),
-                                 ssleep=0, bsleep=0, ...) {
+                      ssleep=0, bsleep=0, ...) {
   
   steps <- length(zs$history)
   atypesmovie <- ifelse(show[1] == 'atypes', TRUE, FALSE)
@@ -14,88 +14,31 @@ movieplot <- function(zs, data, show=c('atypes', 'adata'),
       plot(a, data, ...)
     else
       plot(adata(a), ...)
+    
+    Sys.sleep(bsleep)
   }
-  Sys.sleep(bsleep)
 }
 
-#movieparcoord
 
 
-movieplot.archetypes <- function(zs, data, startscreen=TRUE, ...) {
+moviepcplot <- function(zs, data, show=c('atypes', 'adata'),
+                        ssleep=0, bsleep=0, ...) {
+
   steps <- length(zs$history)
+  atypesmovie <- ifelse(show[1] == 'atypes', TRUE, FALSE)
+  rx <- apply(data, 2, range, na.rm=TRUE)
 
-  # Start screen ...
-  if ( startscreen ) {
-    plot(data, ...)
-    Sys.sleep(2)
-  }
-  
+  Sys.sleep(ssleep)
+
   # ... and play:
   for ( i in seq_len(steps)-1 ) {
-    plot(ahistory(zs, step=i), data, ...)
-    box()
+    a <- ahistory(zs, step=i)
+
+    if ( atypesmovie )
+      pcplot(a, data, ...)
+    else
+      pcplot(adata(a), rx=rx, ...)
+    
+    Sys.sleep(bsleep)
   }
 }
-
-movieplot.data <- function(zs, data, ...) {
-  steps <- length(zs$history)
-
-  # Start screen ...
-  plot(data, type='n', ...)
-  Sys.sleep(2)
-
-  # ... and play:
-  for ( i in seq_len(steps)-1 )
-    plot(adata(ahistory(zs, step=i)), ...)
- 
-}
-
-
-
-# movie.parcoord2.archetypes
-
-# movie.parcoord2.data
-
-
-
-movie <- function(x, ...) {
-  UseMethod('movie')
-}
-
-movie.archetypes <- function(x, data, ...) {
-  zs <- x
-  
-  history <- zs$history
-  steps <- length(history)
-
-  # Start screen ...
-  parcoord2(data, var.label=TRUE, col=gray(0.7), lwd=1)
-  Sys.sleep(2)
-
-  # ... and play:
-  for ( i in (seq_len(steps)-1) ) {
-    h <- history[[paste('s', i, sep='')]]$archetypes
-    parcoord2(h, data)
-  }
-}
-
-
-movie.data <- function(x, data, ...) {
-  zs <- x
-  
-  history <- zs$history
-  steps <- length(history)
-
-  # Start screen ...
-  parcoord2(data, var.label=TRUE, col=0, lwd=0)
-  Sys.sleep(2)
-
-  # ... and play:
-  for ( i in seq_len(steps-1) ) {
-    h <- history[[paste('s', i, sep='')]]$archetypes
-    d <- t(t(atypes(h)) %*% t(h$alphas))
-    parcoord2(d, var.label=TRUE, col=gray(0.7), lwd=1)
-  }
-}
-
-
--- pkg/R/pcplot.R
+++ pkg/R/pcplot.R
@@ -7,20 +7,27 @@ pcplot <- function(x, ...) {
 pcplot.default <- function (x, col=gray(0.7), lty=1, var.label=TRUE,
                             rx=NULL, ...) {
 
-  if ( is.null(rx) )
+  calcrx <- TRUE
+  if ( is.null(rx) ) {
     rx <- apply(x, 2, range, na.rm=TRUE)
+  }
+  else {
+    x <- rbind(rx, x)
+    calcrx <- FALSE
+  }
 
   sx <- sapply(1:ncol(x),
                function(i) {
                  (x[,i] - rx[1,i]) / (rx[2,i] - rx[1,i])
                })
   colnames(sx) <- colnames(x)
+
   x <- sx
   
   matplot(1:ncol(x), t(x), type="l", col=col, lty=lty, 
           xlab="", ylab="", axes=FALSE, ...)
   axis(1, at=1:ncol(x), labels=colnames(x), ...)
-  
+   
   for (i in 1:ncol(x)) {
     lines(c(i, i), c(0, 1), col="grey70")
     if (var.label) 
