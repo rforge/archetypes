63822edc541fe79fee6dc3ec146bf9b5d8ba20b1
manuel <manuel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-11 10:23:15 +0000

    Make 'beside' argument more meaningful.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/archetypes@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/archetypes-barplot.R
+++ pkg/R/archetypes-barplot.R
@@ -3,7 +3,8 @@
 #' Barplot of archetypes.
 #' @param height An \code{\link{archetypes}} object.
 #' @param data The original data matrix.
-#' @param beside Portray the archetypes as stacked bars.
+#' @param beside Show one barplot for each archetype, or one barplot
+#'    with stacked bars, either per archetype or per variable.
 #' @param percentage Show real values or percentages according to the
 #'    original data.
 #' @param ... Passed to the underlying \code{\link{barplot}} call.
@@ -11,8 +12,13 @@
 #' @method barplot archetypes
 #' @importFrom graphics barplot
 #' @export
-barplot.archetypes <- function(height, data, beside=TRUE, percentage=FALSE, ...) {
+barplot.archetypes <- function(height, data,
+                               beside=c('FALSE', 'atypes', 'variables'),
+                               percentage=FALSE, ...) {
+  beside <- match.arg(beside)
+
   atypes <- atypes(height)
+  rownames(atypes) <- sprintf('Archetype %s', seq(length = nrow(atypes)))
 
   if ( !percentage ) {
     ylab <- 'Value'
@@ -23,18 +29,21 @@ barplot.archetypes <- function(height, data, beside=TRUE, percentage=FALSE, ...)
     atypes <- t(t(atypes) / m * 100)
     ylab <- 'Percentage'
     ylim <- c(0,100)
-  }  
+  }
 
-  if ( beside ) {
+  if ( beside == 'variables' ) {
     barplot(atypes, ylab=ylab, beside=TRUE, ylim=ylim, ...)
+  }
+  else if ( beside == 'atypes' ) {
+    barplot(t(atypes), ylab=ylab, beside=TRUE, ylim=ylim, ...)
   }
   else {
     p <- nrow(atypes)
-    
+
     par(mfrow=c(p,1))
-    for ( i in 1:p ) 
+    for ( i in 1:p )
       barplot(atypes[i,], main=paste('Archetype', i),
               ylab=ylab, ylim=ylim, ...)
-  }  
+  }
 }
 
