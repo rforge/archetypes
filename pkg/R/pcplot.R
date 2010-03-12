8a4cac0360259ba7364b0d3630fa53480e7d281d
manuel <manuel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-03-12 10:19:34 +0000

    bug fix.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/archetypes@31 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/pcplot.R
+++ pkg/R/pcplot.R
@@ -48,15 +48,15 @@ pcplot.default <- function (x, col=gray(0.7), lty=1, var.label=TRUE,
   colnames(sx) <- colnames(x)
 
   x <- sx
-  
-  matplot(1:ncol(x), t(x), type="l", col=col, lty=lty, 
+
+  matplot(1:ncol(x), t(x), type="l", col=col, lty=lty,
           xlab="", ylab="", axes=FALSE, ...)
   axis(1, at=1:ncol(x), labels=colnames(x), ...)
-   
+
   for (i in 1:ncol(x)) {
     lines(c(i, i), c(0, 1), col="grey70")
-    if (var.label) 
-      text(c(i, i), c(0, 1), labels=format(rx[, i], digits=3), 
+    if (var.label)
+      text(c(i, i), c(0, 1), labels=format(rx[, i], digits=3),
            xpd=NA, offset=0.3, pos=c(1, 3), cex=0.7)
   }
 }
@@ -77,6 +77,8 @@ lines.pcplot <- function(x, data, col=1, lty=1, ...) {
               function(i) {
                 (x[,i] - rx[1,i]) / (rx[2,i] - rx[1,i])
               })
-  
+  if ( is.vector(x) )
+    x <- matrix(x, nrow = 1)
+
   matlines(1:ncol(x), t(x), col=col, lty=lty, ...)
 }
